ca01b12f254befc638011f28401ff8a09502627b
toanmx0101 <wth8647@gmail.com>
2017-04-06 14:20:20 -0400

    add follow relationship table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
-  has_many :tags         
+  has_many :tags 
+  has_many :relationships        
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405142025) do
+ActiveRecord::Schema.define(version: 20170406180242) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170405142025) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "tags", force: :cascade do |t|
